83d1a9ef58a3256c197432cede8f57507d5c368d
Alan Gutierrez <alan@prettyrobots.com>
2012-07-06 11:14:57 -0400

    Use function names to identify branches.
    Now when a named function is used as a step, the named function is added
    to the context of the cadence. It can be requested by name through step
    function parameters like any other context object.
    
    Instead of receiving the step function itself, however, the user
    receives a wrapper that will record the arguments passed to the step
    function. When the current step function has completed, the wrapper will
    move the cadence to the wrapped step function. The step function will be
    invoked with the recorded parameters.
    
    If the `cadence` function is passed the wrapper as the first argument,
    it will create a callback that will move the cadence to wrapped step
    function if it is invoked. The wrapped step function will be used to
    infer the context names of results given to the callbacks.
    
    We changed the behavior of `cadence` when it is invoked with a direct
    reference to a step function as it's first argument to match that of
    being invoked with a wrapped named step function as the first argument.
    Calling `cadence` with a direct reference to a step function will return
    a callback that will move the cadence to the referenced function if it
    is invoked. The wrapped step function will be used to infer the context
    names of results given to the callbacks.

--- deltree.js
+++ deltree.js
@@ -3,13 +3,13 @@ var deltree = cadence(function (cadence, directory) {
     fs.readdir(directory, cadence());
   }, function (error) {
     if (error.code != "ENOENT") cadence(error);
-    else cadence(null); 
-  }, function shift (files) {
+    else cadence(null, []); 
+  }, function shift (files, rmdir) {
     if (files.length) return path.resolve(directory, files.shift());
-    else cadence(rmdir);
+    else rmdir();
   }, function (file) {
     fs.stat(file, cadence());
-  }, function (stat, file) {
+  }, function (stat, file, shift) {
     if (stat.isDirectory()) deltree(file, cadence(shift));
     else fs.unlink(file, cadence(shift));
   }, function rmdir () {
@@ -22,7 +22,7 @@ var deltree = cadence(function (cadence, directory) {
     fs.readdir(directory, cadence());
   }, function (error) {
     if (error.code != "ENOENT") cadence(error);
-    else cadence(null); 
+    else cadence(null, []); 
   }, function (files) {
     files.forEach(function () {
       cadence(function () {
@@ -35,4 +35,46 @@ var deltree = cadence(function (cadence, directory) {
   }, function exit () {
     fs.rmdir(directory, cadence());
   });
+});
+
+var deltree = cadence(function (cadence, directory)
+  cadence(function () {
+    fs.readdir(directory, cadence());    
+  }, function (error, files) {
+    if (error) {
+      if (error.code != "ENOENT") throw error;
+      cadence(null);
+    } else {
+      files.forEach(function (file, i) {
+        fs.stat(path.resolve(directory, file), cadence({ file: file, stat: cadence(0) }));
+      });
+    }
+  }, function (objects) {
+    objects.forEach(function (object) {
+      if (object.stat.isDirectory()) deltree(object.file, cadence());
+      else fs.unlink(object.file, cadence());
+    }); 
+  }, cadence);
+});
+
+
+var deltree = cadence(function (cadence, directory)
+  cadence(function () {
+    fs.readdir(directory, cadence());    
+  }, function (error, files) {
+    // Messy.
+    if (error) {
+      if (error.code != "ENOENT") throw error;
+      cadence(null);
+    } else {
+      files.forEach(function (file, i) {
+        fs.stat(path.resolve(directory, file), cadence(i));
+      });
+    }
+  }, function (stats, files) {
+    files.forEach(function (file, i) {
+      if (stat[i].isDirectory()) deltree(file, cadence());
+      else fs.unlink(file, cadence());
+    });
+  }, cadence);
 });
