b4d85542fc861992ad4c1f8e0ba4e744dd82d4ec
Alan Gutierrez <alan@prettyrobots.com>
2013-07-08 10:14:24 -0400

    Test finalizers run during explicit exit.
    Closes #113.

--- t/cadence/finalizers.t.js
+++ t/cadence/finalizers.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('proof')(9, function (equal, ok) {
+require('proof')(12, function (equal, ok) {
   var fs = require('fs')
     , cadence = require('../..')
     ;
@@ -36,6 +36,28 @@ require('proof')(9, function (equal, ok) {
       ok(!object.closed, 'leading finalizer open');
       object.used = true;
       return object;
+    });
+
+  })({}, function (error, object) {
+
+    if (error) throw error;
+    ok(object.used, 'leading finalizer was used'); 
+    ok(object.closed, 'leading finalizer was closed'); 
+
+  });
+
+  // Fixup finalizers run after explicit exit.
+  cadence(function (step, object) {
+
+    step(function () {
+      ! function (callback) {
+        callback(null, object);
+      } (step(step, [function (object) { object.closed = true }]));
+    }, function (object) {
+      ok(!object.closed, 'leading finalizer open');
+      object.used = true;
+    }, function () {
+      step(null, object);
     });
 
   })({}, function (error, object) {
