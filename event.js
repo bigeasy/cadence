12837a345926ef007c310926bb8f1e8e216570a8
Alan Gutierrez <alan@prettyrobots.com>
2014-09-17 13:24:40 -0400

    Implement event extension.
    Closes #241.

--- event.js
+++ event.js
@@ -3,9 +3,10 @@ var __slice = [].slice
 function Constructor (options) {
     options = options || {}
     if (this instanceof Constructor) {
-        this._on = options.on || 'on'
-        this._off = options.off || 'removeListener'
-        this._error = options.error || 'error'
+        this._options = {}
+        this._options.on = options.on || 'on'
+        this._options.off = options.off || 'removeListener'
+        this._options.error = options.error || 'error'
     } else {
         return new Constructor(options)
     }
@@ -16,31 +17,36 @@ Constructor.prototype.configure = function (options) {
 }
 
 Constructor.prototype.step = function (step, vargs) {
-    var ee = vargs.shift(), builder = new Builder(step, ee)
+    var ee = vargs.shift(), builder = new Builder(this._options, step, ee)
     if (vargs.length) {
         builder.on.apply(builder, vargs)
     }
     return builder
 }
 
-function Builder (step, ee) {
+function Builder (options, step, ee) {
+    this._options = options
     this._step = step
     this._ee = ee
 }
 
 Builder.prototype.on = function () {
-    var vargs = __slice.call(arguments), step = this._step, name
+    var vargs = __slice.call(arguments),
+        options = this._options, step = this._step, ee = this._ee,
+        name, handler
     if (typeof vargs[0] == 'string') {
         name = vargs.shift()
     }
     if (vargs[0] === Error) {
         if (!name) {
-            name = this._error
+            name = options.error
         }
-        this._ee.on(name, step(Error))
+        handler = step(Error)
     } else {
-        this._ee.on(name, step.apply(null, [ null ].concat(vargs)))
+        handler = step.apply(null, [ null ].concat(vargs))
     }
+    ee[options.on](name, handler)
+    step([function () { ee[options.off](name, handler) }])
     return this
 }
 
