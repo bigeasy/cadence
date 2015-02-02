46dfc8d2dc67642e5bcb68101fe81dc09b275f6b
Alan Gutierrez <alan@prettyrobots.com>
2015-02-02 08:40:13 -0600

    Test loop continue.
    See #284.

--- t/minimal/loop.t.js
+++ t/minimal/loop.t.js
@@ -1,42 +1,50 @@
-require('proof')(2, require('../..')(prove))
+require('proof')(4, prove)
 
-function prove (async, assert) {
+function prove (assert) {
     var cadence = require('../../minimal')
 
     var f = cadence(function (async) {
         var i = 0, loop = async(function () {
             if (++i == 2) return [ loop, i ]
         })()
+    })(function (error, result) {
+        assert(result, 2, 'returned')
     })
 
-    async(function () {
-        f(async())
-    }, function (result) {
-        assert(result, 2, 'returned')
-    }, function () {
-        cadence(function (async) {
-            async(function () {
-                var i = 0, outer = async(function () {
-                    var j = 0, inner = async(function () {
-                        if (i == 2) return [ outer, i, j ]
-                        if (j++ == 2) return [ inner ]
-                    })()
-                    i++
+    cadence(function (async) {
+        async(function () {
+            var i = 0, outer = async(function () {
+                var j = 0, inner = async(function () {
+                    if (i == 2) return [ outer, i, j ]
+                    if (j++ == 2) return [ inner ]
                 })()
-            }, function (i, j) {
-                console.log(i, j)
-                return [ i, j ]
-            })
-        })(async())
-    }, function () {
-        cadence(function (async) {
-            var loop = async(function (i) {
-                if (i == 2) return [ loop, i ]
-                else return i + 1
-            })(0)
-        })(async())
-    }, function (i) {
+                i++
+            })()
+        }, function (i, j) {
+            return [ i, j ]
+        })
+    })(function (error, i, j) {
+        assert([ i, j ], [ 2, 0 ], 'break outer')
+    })
+
+    cadence(function (async) {
+        var loop = async(function (i) {
+            if (i == 2) return [ loop, i ]
+            else return i + 1
+        })(0)
+    })(function (error, i) {
         assert(i, 2, 'loop arguments')
+    })
+
+    cadence(function (async) {
+        var loop = async(function (i) {
+            if (i % 2 == 0) return [ loop(), i + 1 ]
+            else return [ i ]
+        }, function (i) {
+            return [ loop, i ]
+        })(0)
+    })(function (error, i) {
+        assert(i, 1, 'continued')
     })
 }
 
