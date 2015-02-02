912ecff12bfb7c8829bb0a1093c7b3ce8c26f813
Alan Gutierrez <alan@prettyrobots.com>
2015-02-02 08:59:50 -0600

    Coverage of parallel callbacks.
    Closes #284.

---
+++ t/minimal/parallel.t.js
@@ -0,0 +1,15 @@
+require('proof')(1, prove)
+
+function prove (assert) {
+    var cadence = require('../../minimal')
+
+    cadence(function (async) {
+        var first = async()
+        var second = async()
+        second(null, 3)
+        first(null, 1, 2)
+    })(function (error, one, two, three) {
+        if (error) throw error
+        assert([ one, two, three ], [ 1, 2, 3 ], 'parallel')
+    })
+}
