7194f91310f172841bd7cac1ee955f02556f2bf8
Alan Gutierrez <alan@prettyrobots.com>
2015-02-02 08:46:32 -0600

    Test asynchronous operations.
    See #284.

---
+++ t/minimal/async.t.js
@@ -0,0 +1,14 @@
+require('proof')(1, prove)
+
+function prove (assert, callback) {
+    var cadence = require('../../minimal')
+    cadence(function (async) {
+        async(function () {
+            setImmediate(async(), null, 1)
+        })
+    })(function (error, number) {
+        if (error) throw error
+        assert(number, 1, 'async')
+        callback()
+    })
+}
