078a129cad80e058935f42e04bd63475816ff958
Alan Gutierrez <alan@prettyrobots.com>
2015-07-19 10:47:19 -0500

    Add an example of debugging using `superstack`.

---
+++ notes/superstack.js
@@ -0,0 +1,19 @@
+require('superstack')
+
+Error.stackTraceLimit = Infinity
+
+var cadence = require('../redux')
+
+var f = cadence(function (async) {
+    async(function () {
+        setImmediate(async())
+    }, function () {
+        setTimeout(async(), 0)
+    }, function () {
+        throw new Error('explode')
+    })
+})
+
+f(function (error) {
+    console.log(error.stack)
+})
