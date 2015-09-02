d21a4e216b45053627173e3349a5aeda376df18f
Alan Gutierrez <alan@prettyrobots.com>
2015-09-02 01:30:07 -0500

    Deprecate `async.ee`.
    Closes #329.

--- cadence.js
+++ cadence.js
@@ -358,57 +358,6 @@ function cadence () {
     return f
 }
 
-function Sink (async, self, ee) {
-    this._async = async
-    this._self = self
-    this._ee = ee
-    this._listeners = []
-    this._callback = async()
-}
-
-Sink.prototype._register = function (event, fn) {
-    this._ee.on(event, fn)
-    this._listeners.push({ event: event, fn: fn })
-}
-
-Sink.prototype.error = function (filter) {
-    this._register('error', function (error) {
-        if (filter) {
-            error = call(filter, this._self, [ error ])[1]
-        }
-        if (error) {
-            this._terminate([ error ])
-        }
-    }.bind(this))
-    return this
-}
-
-Sink.prototype.end = function (event) {
-    this._register(event, variadic(function (vargs) {
-        vargs.unshift(null)
-        this._terminate(vargs)
-    }, this))
-    return this
-}
-
-Sink.prototype._terminate = function (vargs) {
-    for (var i = 0, I = this._listeners.length; i < I; i++) {
-        var listener = this._listeners[i]
-        this._ee.removeListener(listener.event, listener.fn)
-    }
-    this._callback.apply(null, vargs)
-}
-
-Sink.prototype.on = function (event, listener) {
-    this._register(event, variadic(function (vargs) {
-        var ret = call(listener, this._self, vargs)
-        if (ret.length === 2) {
-            this._terminate([ ret[1] ])
-        }
-    }, this))
-    return this
-}
-
 function variadic (f, self) {
     return function () {
         var I = arguments.length
@@ -421,8 +370,7 @@ function variadic (f, self) {
 }
 
 async.ee = function (ee) {
-    var async = this
-    return new Sink(this, async.self, ee)
+    throw new Error('deprecated: use `Delta`.')
 }
 
 async.forEach = variadic(function (steps) {
--- t/cadence/sink.t.js
+++ t/cadence/sink.t.js
@@ -1,54 +1,11 @@
-require('proof')(4, prove)
+require('proof')(1, prove)
 
 function prove (assert) {
     var cadence = require('../..')
-
     var events = require('events')
-
-    var ee = new events.EventEmitter
-
-    var f = cadence(function (async) {
-        async.ee(ee).end('end')
-                    .on('data', function (data) {
-                        if (data == 2) {
-                            throw new Error('data')
-                        }
-                    })
-                    .error(function (error) {
-                        if (error.message != 'catch') {
-                            throw error
-                        }
-                    })
+    cadence(function (async) {
+        async.ee(new events.EventEmitter)
+    })(function (error) {
+        assert(error.message, 'deprecated: use `Delta`.', 'deprecated')
     })
-
-    f(function (error, number) {
-        if (error) throw error
-        assert(number, 1, 'end')
-    })
-
-    ee.emit('error', new Error('catch'))
-    ee.emit('end', 1)
-
-    f(function (error) {
-        assert(error.message, 'rethrow', 'thrown')
-    })
-
-    ee.emit('error', new Error('rethrow'))
-
-    f(function (error) {
-        assert(error.message, 'data', 'thrown')
-    })
-
-    ee.emit('data', 1)
-    ee.emit('data', 2)
-
-    var e = cadence(function (async) {
-        async.ee(ee).error()
-    })
-
-    e(function (error) {
-        assert(error.message, 'catch', 'basic error')
-    })
-
-    ee.emit('error', new Error('catch'))
 }
